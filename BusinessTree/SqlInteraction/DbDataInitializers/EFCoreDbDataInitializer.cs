49ca0903b7dfd50303b24ebea551721a2a92ebc0
Igor <desert.ice10@gmail.com>
2018-07-08 19:47:25 +0300

    Created DbInitializer class for inserting data into db

---
+++ BusinessTree/SqlInteraction/DbDataInitializers/EFCoreDbDataInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlInteraction.DbDataInitializers
+{
+    public class EFCoreDbDataInitializer
+    {
+        private SS2018Context dbContext;
+        public EFCoreDbDataInitializer(SS2018Context context)
+        {
+            dbContext = context;
+        }
+
+        public void InsertLevels(int majorLevelsAmount, int childLevelsAmount,int nestingPow)
+        {
+            for(int i = 0; i < majorLevelsAmount; i++)
+            {
+                for(int j = 0; j < nestingPow; j++)
+                {
+
+                }
+            }
+        }
+    }
+}
